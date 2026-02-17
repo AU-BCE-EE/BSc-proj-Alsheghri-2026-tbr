ec6d399a6d918a53e6c13b59cebd7c6e13e7740d
osalsheghri <osman.alsheghri@gmail.com>
2026-02-17 13:20:03 +0100

    Changed names of input parameters
    The names was changed so they do not get mixed with other parameters in the model module, i also added docstrings to the functions

--- mods/speciation_model.py
+++ mods/speciation_model.py
@@ -1,16 +1,46 @@
 import numpy as np
 from scipy.optimize import root_scalar
 # here is a function with same form as Sasha's but without the matrix...
-def spec(TOTC,K1,K2,KW):
+def spec(TOTC,K_hco3,K_co3,KW):
+    """
+    Calculate carbonate system speciation and pH from total carbonate
+    concentration
+
+    Author: Osman Alsheghri
+
+    Parameters/Input
+    -------------
+    TOTC  : float
+            Total carbonate concentration (mol/m3) = [H2CO3] + [HCO3^-] + [CO3^2-]
+
+    K_hco3: float
+            Equilibrium constant for H2CO3 ⇌ HCO3^- + H^+
+
+    K_co3 : float
+            Equilibrium constant for HCO3^- ⇌ CO3 ^2-  + H^+
+
+    KW    : float
+            Water dissociation constant 
+
+    Returns
+    ----------
+    Dictionary
+        'c_h2co3' : float - H2CO3 concentration     (mol/m3)
+        'c_hco3'  : float - HCO3^- concentration    (mol/m3)
+        'c_co3'   : float - CO3^2- concentration    (mol/m3)
+        'c_oh'    : float - OH^- concentration      (mol/m3)
+        'c_h'     : float - H^+ concentration       (mol/m3)
+        'pH'      : float - pH value
+    """
     def residC(c_h2co3):
         def residH(c_h):
-            c_hco3  = (K1*c_h2co3)/c_h
-            c_co3   = (K1*K2*c_h2co3)/c_h**2
+            c_hco3  = (K_hco3*c_h2co3)/c_h
+            c_co3   = (K_hco3*K_co3*c_h2co3)/c_h**2
             c_oh    = KW/c_h
             return c_h - c_hco3 - 2*c_co3-c_oh
         c_h     = root_scalar(residH, bracket=[1E-14, 1], method='brentq').root
-        c_hco3  = (K1*c_h2co3)/c_h
-        c_co3   = (K1*K2*c_h2co3)/c_h**2
+        c_hco3  = (K_hco3*c_h2co3)/c_h
+        c_co3   = (K_hco3*K_co3*c_h2co3)/c_h**2
         c_oh    = KW/c_h
         return TOTC - c_h2co3 - c_hco3 - c_co3, c_h, c_hco3,c_co3,c_oh
     c_h2co3 = root_scalar(lambda x: residC(x)[0], bracket=[1E-10, TOTC], method='brentq').root
@@ -28,22 +58,58 @@ def spec(TOTC,K1,K2,KW):
     return out
 
 
+
+
+
+
+
+
 # Sasha said maybe we could find an algerbraic equation for c_h2co3 instead
 # of solving it numerically.
 # Maybe like this? where we use the TOTC equation to isolate c_h2co3, then 
 # we plug it in the charge balance equation, this makes everything depend only of c_h??
 # I actually did exactly what has been done in Sasha's R model to handle algebraic c_h2co3 :) 
-def spec2(TOTC,K1,K2,KW):
+def spec2(TOTC,K_hco3,K_co3,KW):
+    """
+    Calculate carbonate system speciation and pH from total carbonate
+    concentration
+
+    Author: Osman Alsheghri
+
+    Parameters/Input
+    -------------
+    TOTC  : float
+            Total carbonate concentration (mol/m3) = [H2CO3] + [HCO3^-] + [CO3^2-]
+
+    K_hco3: float
+            Equilibrium constant for H2CO3 ⇌ HCO3^- + H^+
+
+    K_co3 : float
+            Equilibrium constant for HCO3^- ⇌ CO3 ^2-  + H^+
+
+    KW    : float
+            Water dissociation constant 
+
+    Returns
+    ----------
+    Dictionary
+        'c_h2co3' : float - H2CO3 concentration     (mol/m3)
+        'c_hco3'  : float - HCO3^- concentration    (mol/m3)
+        'c_co3'   : float - CO3^2- concentration    (mol/m3)
+        'c_oh'    : float - OH^- concentration      (mol/m3)
+        'c_h'     : float - H^+ concentration       (mol/m3)
+        'pH'      : float - pH value
+    """
     def residH(c_h):
-            c_h2co3 = TOTC/(1 + K1/c_h + (K2*K1)/c_h**2)
-            c_hco3  = (K1*c_h2co3)/c_h
-            c_co3   = (K1*K2*c_h2co3)/c_h**2
+            c_h2co3 = TOTC/(1 + K_hco3/c_h + (K_co3*K_hco3)/c_h**2)
+            c_hco3  = (K_hco3*c_h2co3)/c_h
+            c_co3   = (K_hco3*K_co3*c_h2co3)/c_h**2
             c_oh    = KW/c_h
             return c_h -  c_hco3 - 2*c_co3-c_oh
     c_h     = root_scalar(residH, bracket=[1E-14, 1], method='brentq').root
-    c_h2co3 = TOTC/(1 + K1/c_h + (K2*K1)/c_h**2)
-    c_hco3  = (K1*c_h2co3)/c_h
-    c_co3   = (K1*K2*c_h2co3)/c_h**2
+    c_h2co3 = TOTC/(1 + K_hco3/c_h + (K_co3*K_hco3)/c_h**2)
+    c_hco3  = (K_hco3*c_h2co3)/c_h
+    c_co3   = (K_hco3*K_co3*c_h2co3)/c_h**2
     c_oh    = KW/c_h
     pH      = - np.log10(c_h) 
     out = {
@@ -57,10 +123,47 @@ def spec2(TOTC,K1,K2,KW):
     return out
 
 
+
+
+
+
+
+
+
 # the same as spec2 but here with matrix apporach. When we are 100% sure it works we could just
 # remove the two above or have a module with matrix approach only.  
 
-def spec2_matrix(TOTC,K1,K2,KW):
+def spec2_matrix(TOTC,K_hco3,K_co3,KW):
+     """
+    Calculate carbonate system speciation and pH from total carbonate
+    concentration
+
+    Author: Osman Alsheghri
+
+    Parameters/Input
+    -------------
+    TOTC  : float
+            Total carbonate concentration (mol/m3) = [H2CO3] + [HCO3^-] + [CO3^2-]
+
+    K_hco3: float
+            Equilibrium constant for H2CO3 ⇌ HCO3^- + H^+
+
+    K_co3 : float
+            Equilibrium constant for HCO3^- ⇌ CO3 ^2-  + H^+
+
+    KW    : float
+            Water dissociation constant 
+
+    Returns
+    ----------
+    Dictionary
+        'c_h2co3' : float - H2CO3 concentration     (mol/m3)
+        'c_hco3'  : float - HCO3^- concentration    (mol/m3)
+        'c_co3'   : float - CO3^2- concentration    (mol/m3)
+        'c_oh'    : float - OH^- concentration      (mol/m3)
+        'c_h'     : float - H^+ concentration       (mol/m3)
+        'pH'      : float - pH value
+     """
     # From the equilibriums we know that 
     # HCO3 = K1 * c_h2co3**1 * c_h**-1 
     # CO3  = K1*K2 * c_h2co3**1 * c_h **-2 
@@ -77,7 +180,7 @@ def spec2_matrix(TOTC,K1,K2,KW):
           [-1 , 2], # CO3^2-
           [0 , 1]  # OH^-
      ])
-     K = np.array([K1,K1*K2,KW])
+     K = np.array([K_hco3,K_hco3*K_co3,KW])
      def residH(c_h):
           # denominator for c_h2co3 equation --> denom = 1 + K1/c_h + K1*K2/c_h**2  
           denom = 1 + np.sum(K * (c_h**(-S[:,1])) * (- S[:,0])) # if we did not have the last part
